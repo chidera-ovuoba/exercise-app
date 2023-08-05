f24960499e3ad9bf7cf05dc209f65ae4aeb9d742
chidera-ovuoba <107512707+chidera-ovuoba@users.noreply.github.com>
2023-08-05 17:26:21 +0100

    9/5/2023

--- src/components/ExerciseCard.js
+++ src/components/ExerciseCard.js
@@ -2,7 +2,9 @@ import React from 'react'
 import { Link } from 'react-router-dom';
 import { Button, Stack, Typography } from '@mui/material';
 const ExerciseCard = ({ exercise, setIsBodyPart }) => {
-    // ;
+
+
+    
   return (
       <Link className='exercise-card' to={`/exercise/${exercise.id}`} onClick={()=>setIsBodyPart(false)}>
           <img src={exercise.gifUrl} alt={exercise.name} loading='lazy' />
